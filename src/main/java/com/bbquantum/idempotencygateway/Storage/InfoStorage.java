9f0cb0e9d193e050deb81ab4444b6cb75188b1e9
Benney-Walker <benjaminbadubenney@gmail.com>
2026-03-12 18:10:47 +0000

    Added info storage class for storing transaction data

---
+++ src/main/java/com/bbquantum/idempotencygateway/Storage/InfoStorage.java
@@ -0,0 +1,24 @@
+package com.bbquantum.idempotencygateway.Storage;
+
+import com.bbquantum.idempotencygateway.DTOs.StoredInfo;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class InfoStorage {
+
+    private final ConcurrentHashMap<String, StoredInfo> storedInfo = new ConcurrentHashMap<>();
+
+    public StoredInfo getStoredInfo(String key) {
+        return storedInfo.get(key);
+    }
+
+    public void setStoredInfo(String key, StoredInfo storedInfo) {
+        this.storedInfo.put(key, storedInfo);
+    }
+
+    public boolean contains(String key) {
+        return storedInfo.containsKey(key);
+    }
+}
