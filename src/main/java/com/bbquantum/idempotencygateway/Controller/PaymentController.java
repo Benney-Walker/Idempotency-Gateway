56c28e72e83170631d3ee5ecd7f4836df924856b
Benney-Walker <benjaminbadubenney@gmail.com>
2026-03-12 18:50:34 +0000

    Added Rest Controller to receive API calls

---
+++ src/main/java/com/bbquantum/idempotencygateway/Controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.bbquantum.idempotencygateway.Controller;
+
+import com.bbquantum.idempotencygateway.DTOs.PaymentRequest;
+import com.bbquantum.idempotencygateway.Service.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public ResponseEntity<?> processPayment(@RequestHeader("Idempotency-Key") String key,
+                                            @RequestBody PaymentRequest paymentRequest) throws InterruptedException {
+
+        return paymentService.processPayment(key, paymentRequest);
+    }
+}
